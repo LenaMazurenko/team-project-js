6c8dd976790c0cf20b931a5cdab56af8231ce5b2
LenaMazurenko <lena@rent-it.su>
2021-11-27 13:53:43 +0200

    update wead/write to FB

--- src/js/movieModal.js
+++ src/js/movieModal.js
@@ -1,6 +1,5 @@
 import movieModal from '../templates/movie-modal.hbs';
-import { User } from './user-class';
-import { user } from './auth';
+import { readFromFBHundler, writeToFBHundler } from './read-write-to-firebase';
 //========================================================
 
 //=====================================================
@@ -34,55 +33,17 @@ export default class MovieModal {
       if (e.target === e.currentTarget) this.closeModal();
     });
     //read from FB
-    this.refs.watchedBtn.addEventListener('click', readFromFB);
+    this.refs.watchedBtn.addEventListener('click', () => {
+      writeToFBHundler('watched');
+    });
 
     //write to FB
-    this.refs.queueBtn.addEventListener('click', writeToFB);
+    this.refs.queueBtn.addEventListener('click', () => {
+      writeToFBHundler('queue');
+    });
   }
 
   closeModal() {
     this.refs.modal.remove();
   }
-}
-function readFromFB() {
-  if (!user.isLogin) {
-    console.log('нет данных');
-    return;
-  }
-  return fetch(
-    `https://gitpodmy-default-rtdb.europe-west1.firebasedatabase.app/collection/${user.idLocal}.json?auth=${user.id}`,
-  )
-    .then(response => response.json())
-    .then(response => {
-      if (response && response.error) {
-        console.log('ошибка чтения из FB');
-        //`<p class="error">${response.error}</p>`;
-      }
-      console.log(response);
-    });
-}
-
-///////////////////////////////////////////////////////////////////////////////
-const test = {
-  nameFilm: 'llllll',
-  rating: '0000000',
-  cost: 'yyyyyyy',
-};
-function writeToFB() {
-  if (!user.isLogin) {
-    console.log('вы не зарегистрировны!');
-    return;
-  }
-  return fetch(
-    `https://gitpodmy-default-rtdb.europe-west1.firebasedatabase.app/collection/${user.idLocal}.json`,
-    {
-      method: 'POST',
-      body: JSON.stringify(test),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    },
-  )
-    .then(response => response.json())
-    .then(data => console.log(data));
 }
---
+++ src/js/read-write-to-firebase.js
@@ -0,0 +1,44 @@
+import { user } from './auth';
+
+export function readFromFBHundler() {
+  if (!user.isLogin) {
+    console.log('нет данных');
+    return;
+  }
+  return fetch(
+    `https://gitpodmy-default-rtdb.europe-west1.firebasedatabase.app/collection/${user.idLocal}.json?auth=${user.id}`,
+  )
+    .then(response => response.json())
+    .then(response => {
+      if (response && response.error) {
+        console.log('ошибка чтения из FB');
+        //`<p class="error">${response.error}</p>`;
+      }
+      console.log(response);
+    });
+}
+
+///////////////////////////////////////////////////////////////////////////////
+const test = {
+  nameFilm: 'llllll',
+  rating: '0000000',
+  cost: 'yyyyyyy',
+};
+export function writeToFBHundler(nameCollection) {
+  if (!user.isLogin) {
+    console.log('вы не зарегистрировны!');
+    return;
+  }
+  return fetch(
+    `https://gitpodmy-default-rtdb.europe-west1.firebasedatabase.app/collection/${user.idLocal}/${nameCollection}.json`,
+    {
+      method: 'POST',
+      body: JSON.stringify(test),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  )
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
